2d7c70b6b7d8bc2524b3ea2ad6d10f1eb9118047
arcolinuxz <arcolinuxinfo@gmail.com>
2022-08-30 20:01:43 +0200

    fg

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get('/', (req, res) => {
     res.sendFile("index.html", { root: __dirname });
 });
 api.get("/ics/:code", (req, res) => { // cache 5 minutes cache(5*60*1000)
+    req.params.code=req.params.code.replace('.ics','')
     console.log(req)
     downloaderics(req.params.code, res);
 })
