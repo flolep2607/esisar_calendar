09a5a495ed98db11ed091e09ac9920d10c72ba94
arcolinuxz <arcolinuxinfo@gmail.com>
2022-08-30 17:44:32 +0200

    up

--- index.js
+++ index.js
@@ -3,37 +3,44 @@ const app = express();
 const fs = require("fs");
 const api = express.Router();
 const apicache = require("apicache");
-const { 
+const {
     downloaderics,
-    downloader 
+    downloader
 } = require("./downloader.js");
 let cache = apicache.options({
-    enabled:true
-  }).middleware
+    enabled: true
+}).middleware
 
-const SALLES="9756,2432,2101,2895,2336,1814,1660,2706,2295,3098,3096,9757,15186,1796,1797,2543";
+const SALLES = "9756,2432,2101,2895,2336,1814,1660,2706,2295,3098,3096,9757,15186,1796,1797,2543";
+app.use((req, res, next) => {
+    if (req.headers.host.indexOf('localhost') < 0 && (req.headers['x-forwarded-proto'] || '').toLowerCase() !== 'https') {
+        res.redirect('https://' + req.headers.host + req.url);
+    } else {
+        next();
+    }
+})
 app.get('/', (req, res) => {
     res.sendFile("index.html", { root: __dirname });
 });
 api.use(cache('5 minutes'))
 api.get("/ics/:code", (req, res) => { // cache 5 minutes cache(5*60*1000)
-    downloaderics(req.params.code,res);
+    downloaderics(req.params.code, res);
 })
 api.get("/json/:code", (req, res) => { // cache 5 minutes
-    if(req.params.code=="salles"){
+    if (req.params.code == "salles") {
         // fs.readFile("./classes.json", "utf8", (err, data) => {
         //     if (err) throw err;
         //     // data=JSON.parse(data);
         //     // const query=data.map(c=>c.code);
-            
+
         // });
-        downloader(SALLES,res,false);
-    }else{
-        downloader(req.params.code,res);
+        downloader(SALLES, res, false);
+    } else {
+        downloader(req.params.code, res);
     }
 })
 api.get("/json", (req, res) => { // cache 5 minutes
-    downloader(req.params.code,res);
+    downloader(req.params.code, res);
 })
 api.get("/classes", (_, res) => { // cache 5 minutes
     fs.readFile("./classes.json", "utf8", (err, data) => {
@@ -45,7 +52,7 @@ api.get("/classes", (_, res) => { // cache 5 minutes
 api.get("/salles", (_, res) => { // cache 5 minutes
     fs.readFile("./classes.json", "utf8", (err, data) => {
         if (err) throw err;
-        downloader(SALLES,res,false);
+        downloader(SALLES, res, false);
     });
 });
 
