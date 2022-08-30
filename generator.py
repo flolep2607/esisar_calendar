979f5639e65478def3b47f855e98a608483b78c7
flolep2607 <flo.lep2607@gmail.com>
2022-08-30 03:08:26 +0200

    easy update

--- generator.py
+++ generator.py
@@ -8,20 +8,21 @@ import base64
 parser=etree.HTMLParser(recover=True)
 ret=r"([1-5]A|Etudiants|Esisar)"
 ret2=r"([1-5]A)"
+START_YEAR=2022
 session=requests.Session()
 session.headers.update({
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
     'Accept-Encoding': 'deflate',
-    'Referer': 'https://edt.grenoble-inp.fr/2021-2022/esisar/etudiant/jsp/standard/gui/tree.jsp?forceLoad=false&isDirect=true',
+    'Referer': 'https://edt.grenoble-inp.fr/2022-2023/esisar/etudiant/jsp/standard/gui/tree.jsp?forceLoad=false&isDirect=true',
     "Authorization":"Basic %s"%base64.b64encode((os.getenv('EDT_USER')+":"+os.getenv('EDT_PASSWORD')).encode()).decode()
 })
 CODES=[]
 def get(code):
     if code in CODES:return
     CODES.append(code)
-    page = session.get('https://edt.grenoble-inp.fr/2021-2022/esisar/etudiant/jsp/standard/gui/tree.jsp?branchId=%s&expand=false&forceLoad=false&reload=false&scroll=0'%code)
+    page = session.get(f'https://edt.grenoble-inp.fr/{START_YEAR}-{START_YEAR+1}/esisar/etudiant/jsp/standard/gui/tree.jsp?branchId=%s&expand=false&forceLoad=false&reload=false&scroll=0'%code)
     tree = etree.fromstring(page.content,parser=parser)
     for elem in tree.cssselect('span.treebranch a'):
         if re.match(ret,elem.text):
@@ -32,8 +33,8 @@ def get(code):
             code=elem.get("href").split('checkBranch(')[1].split(',')[0]
             get(code)
 
-response = session.get('https://edt.grenoble-inp.fr/2021-2022/esisar/etudiant')
-page = session.get('https://edt.grenoble-inp.fr/2021-2022/esisar/etudiant/jsp/standard/gui/tree.jsp?category=trainee&expand=true&forceLoad=false&reload=false&scroll=0')
+response = session.get(f'https://edt.grenoble-inp.fr/{START_YEAR}-{START_YEAR+1}/esisar/etudiant')
+page = session.get(f'https://edt.grenoble-inp.fr/{START_YEAR}-{START_YEAR+1}/esisar/etudiant/jsp/standard/gui/tree.jsp?category=trainee&expand=true&forceLoad=false&reload=false&scroll=0')
 tree = etree.fromstring(page.content,parser=parser)
 for elem in tree.cssselect('span.treebranch a'):
     print(elem.text)
@@ -44,7 +45,7 @@ for elem in tree.cssselect('span.treebranch a'):
 
 
 get("trainee")
-page = session.get('https://edt.grenoble-inp.fr/2021-2022/esisar/etudiant/jsp/standard/gui/tree.jsp')
+page = session.get(f'https://edt.grenoble-inp.fr/{START_YEAR}-{START_YEAR+1}/esisar/etudiant/jsp/standard/gui/tree.jsp')
 tree = etree.fromstring(page.content,parser=parser)
 classes=[]
 for elem in tree.cssselect('div.treeline span a'):
